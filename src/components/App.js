9c129b832a90ba073f48c7a1d39ad8ff8bd1ea06
subajat1 <bayumunajat@outlook.com>
2019-05-20 06:53:46 +0800

    fix pokemon list and cell components

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PokeList from './PokeList';
 import './styles/App.css';
 
 
@@ -12,7 +13,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <PokeList />
       </div>
     );
   }
