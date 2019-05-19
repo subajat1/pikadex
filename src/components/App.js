c8e54ab35a232b11b823aaa0c12c5331df5e6666
subajat1 <bayumunajat@outlook.com>
2019-05-20 06:37:24 +0800

    fix app and style

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,16 @@ import './styles/App.css';
 
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {};
+  }
+
   render() {
     return (
       <div className="App">
-        hellohello
+
       </div>
     );
   }
