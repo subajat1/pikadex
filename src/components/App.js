2014a1a7583d57d51090b34e05f5f4d38dba1f67
subajat1 <bayumunajat@outlook.com>
2019-05-20 07:40:02 +0800

    develop api connector

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import PokeList from './PokeList';
+import DetailView from './DetailView';
+import Pokemon from '../Pokemon';
 import './styles/App.css';
 
 
@@ -7,13 +9,34 @@ class App extends Component {
 
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      pokemon: {}
+    };
+    
+    this.handleOnClick = this.handleOnClick.bind(this);
   }
+
+  handleOnClick(id) {
+    const proxyurl = `http://localhost:3000/`;
+    const url = `http://pokeapi.co/api/v2/pokemon/${id}/`;
+
+    fetch(url)
+    .then(res => res.json())
+    .then(data => {
+      const pokemon = new Pokemon(data);
+
+      console.log(pokemon);
+      this.setState({ pokemon });
+    })
+    .catch(err => console.log(err));
+  }
+
 
   render() {
     return (
       <div className="App">
-        <PokeList />
+        <PokeList handleOnClick={this.handleOnClick} />
+        <DetailView pokemon={this.state.pokemon} />
       </div>
     );
   }
